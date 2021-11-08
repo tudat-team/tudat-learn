e73454a18378d909aea8a5694b68ab334ae8d93a
Francisco Castanheira <franciscocastanheira1998@gmail.com>
2021-11-08 17:25:01 +0100

    Added an estimators directory for classifiers, regressors and clustering. Also added the regressors directory inside estimators, with files to be used by rbfn.

---
+++ cpp/include/estimators/regressor.h
@@ -0,0 +1,24 @@
+/*    Copyright (c) 2010-2018, Delft University of Technology
+ *    All rights reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ */
+
+#ifndef TUDAT_LEARN_REGRESSOR_H
+#define TUDAT_LEARN_REGRESSOR_H
+
+#include "estimator.h"
+
+namespace tudat_learn
+{
+  
+class Regressor : public Estimator {};
+  
+} // namespace tudat_learn
+
+
+#endif // TUDAT_LEARN_REGRESSOR_H
